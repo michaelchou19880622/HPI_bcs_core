395db11e225eceb4481fe37d0a3bb43995f1c650
arnorqq <arnorqq@gmail.com>
2019-10-22 16:02:53 +0800

    把richmenu_new改成richmenu

--- src/main/java/com/hpicorp/core/repository/RichMenuRepository.java
+++ src/main/java/com/hpicorp/core/repository/RichMenuRepository.java
@@ -12,16 +12,16 @@ import com.hpicorp.core.entities.RichMenu;
 
 public interface RichMenuRepository extends JpaRepository<RichMenu, Long> {
 	
-	@Query(value = "select * from richmenu_new where level = '1' and start_date > now()", nativeQuery = true)
+	@Query(value = "select * from richmenu where level = '1' and start_date > now()", nativeQuery = true)
 	public List<RichMenu> findRichMenuOnSchedule();
 	
-	@Query(value = "select * from richmenu_new where group_id = :groupId", nativeQuery = true)
+	@Query(value = "select * from richmenu where group_id = :groupId", nativeQuery = true)
 	public List<RichMenu> findOldRichMenu(@Param("groupId") Long groupId);
 	
 	// 因為圖文選單 Default 在生效後只能保存一個，必須將舊的查詢出來
 	public List<RichMenu> findByGroupIdAndStartDateLessThan(Long groupId, Date now);
 	
-	@Query(value = "select * from richmenu_new where level = '1' ", nativeQuery = true)
+	@Query(value = "select * from richmenu where level = '1' ", nativeQuery = true)
 	public List<RichMenu> findByLevel();
 
 	public Optional<RichMenu> findByGroupId(Long string);
